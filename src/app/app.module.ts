06ac578e04f9cd2ebb11c7c6fff13bb9802ea00e
jenam15 <amjen.dang@gmail.com>
2022-04-05 13:52:11 +0200

    ng_ws1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
